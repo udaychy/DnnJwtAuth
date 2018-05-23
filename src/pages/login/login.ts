30f5343efe1a765035485627bdea703ab4641819
udaychy <uday.choudhary.hsetin@gmail.com>
2018-05-23 15:57:34 +0530

    Add FormBuilder for model driven login form

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,44 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { AuthProvider } from '../../providers/auth/auth';
-import { LoginResponse } from '../../model/login-response';
 import { HomePage } from '../home/home';
 import { CommonProvider } from '../../providers/common/common';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html',
 })
 export class LoginPage {
-  userName: string;
-  password: string
-  
+  private loginForm: FormGroup
+
   constructor(public navCtrl: NavController,
     private authService: AuthProvider,
-    private commonService: CommonProvider) {
-
+    private commonService: CommonProvider,
+    private formBuilder: FormBuilder) {
+      this.loginForm = this.formBuilder.group({
+        userName: new FormControl('', Validators.required),
+        password: new FormControl('', Validators.required),
+      });
   }
 
   login() {
-    if (!this.userName || !this.password) {
+    console.log(this.loginForm.value)
+
+    if (!this.loginForm.value.userName || !this.loginForm.value.password) {
       this.commonService.presentToaster("Please provide both username and password");
       return;
     }
 
     this.commonService.displayLoader("Logging In...").then(loader => {
-      this.authService.login(this.userName, this.password).then(authData => {
+      this.authService.login(this.loginForm.value.userName, this.loginForm.value.password).then(authData => {
         loader.dismiss();
         if(authData.userId > 0){
           this.commonService.presentToaster("Successfully Logged In");
           this.setHomeAsRoot(authData.userId);
         }else{
           this.commonService.presentToaster("Login Failed");
-          this.password= "";
+          this.loginForm.controls['password'].reset();
         }
       }).catch(rej => {
         loader.dismiss();
         this.commonService.presentToaster("Login Failed");
-        this.password= "";
+        this.loginForm.controls['password'].reset();
         console.log(rej);
       })
     })
